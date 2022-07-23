d248a09f1248deb7d049a2df7e38c4b89cf2ae3c
KunalRathodHere <77500698+KunalRathodHere@users.noreply.github.com>
2022-07-23 15:10:46 +0530

    Time: 1 ms (88.94%), Space: 40.1 MB (95.26%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -1,63 +1,79 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
         
-        int i = 0;
-        int j = 0;
+           String[] levels1 = version1.split("\\.");
+    String[] levels2 = version2.split("\\.");
+    
+    int length = Math.max(levels1.length, levels2.length);
+    for (int i=0; i<length; i++) {
+    	Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
+    	Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
+    	int compare = v1.compareTo(v2);
+    	if (compare != 0) {
+    		return compare;
+    	}
+    }
+    
+    return 0;
         
-        String[] a = version1.split("\\.");
-        String[] b = version2.split("\\.");
         
-        System.out.println(a.length + " " + b.length);
+//         int i = 0;
+//         int j = 0;
         
-        while(i < a.length && j < b.length){
+//         String[] a = version1.split("\\.");
+//         String[] b = version2.split("\\.");
+        
+//         System.out.println(a.length + " " + b.length);
+        
+//         while(i < a.length && j < b.length){
             
-            int tempa = Integer.valueOf(a[i]);
-            int tempb = Integer.valueOf(b[j]);
+//             int tempa = Integer.valueOf(a[i]);
+//             int tempb = Integer.valueOf(b[j]);
             
-            System.out.println(tempa + " " + tempb);
+//             System.out.println(tempa + " " + tempb);
             
-            if(tempa == tempb){
+//             if(tempa == tempb){
                 
                 
                 
-                i++;
-                j++;
-            }
+//                 i++;
+//                 j++;
+//             }
             
-            if(tempa > tempb){
-                return 1;
-            }
-            if(tempa < tempb){
-                return -1;
-            }
+//             if(tempa > tempb){
+//                 return 1;
+//             }
+//             if(tempa < tempb){
+//                 return -1;
+//             }
             
-        }
+//         }
         
-        if((i == a.length -1) && (j == b.length -1)){
+//         if((i == a.length -1) && (j == b.length -1)){
             
-            System.out.println("here i am");
-            return 0;
+//             System.out.println("here i am");
+//             return 0;
             
-        }
+//         }
         
-        while(i < a.length){
+//         while(i < a.length){
             
-            if(Integer.valueOf(a[i]) > 0)
-                return 1;
-            i++;
+//             if(Integer.valueOf(a[i]) > 0)
+//                 return 1;
+//             i++;
             
-        }
+//         }
         
-        while(j < b.length){
-            System.out.println("j = " + j);
-            if(Integer.valueOf(b[j]) >0)
-                return -1;
-            j++;
-        }
+//         while(j < b.length){
+//             System.out.println("j = " + j);
+//             if(Integer.valueOf(b[j]) >0)
+//                 return -1;
+//             j++;
+//         }
         
         
         
-        return 0;
+//         return 0;
     }
     
 }
