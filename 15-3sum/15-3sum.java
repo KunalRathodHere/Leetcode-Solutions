3ba66615f2c96012dc51aab26b7364898b5ea44a
KunalRathodHere <77500698+KunalRathodHere@users.noreply.github.com>
2022-09-24 19:44:22 +0530

    Time: 44 ms (37.12%), Space: 64.1 MB (24.18%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -16,7 +16,9 @@ class Solution {
         for(int i =0; i<nums.length; i++){
             
             int curr = nums[i];
-            
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
             twoSum(nums, -1*curr, i+1);
             
         }
