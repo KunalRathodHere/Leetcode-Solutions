8e27c3d368664cb2064a90a10eaa587bec800cfd
KunalRathodHere <77500698+KunalRathodHere@users.noreply.github.com>
2023-06-09 12:43:48 +0530

    Added solution - LeetHub

--- Transform String - GFG/transform-string.java
+++ Transform String - GFG/transform-string.java
@@ -29,37 +29,63 @@ class Solution
     int transform (String A, String B)
     {
         // code here
+        if(A.length() != B.length()) return -1;
         
-        int[] arr = new int[256];
-        if(A.length() != B.length()){
-            return -1;
+        int i = A.length()-1;
+        int j = B.length() - 1;
+        int cnt = 0;
+        
+        int count[] = new int[256];
+ 
+        // count characters in A
+        // subtract count for every character in B
+        for (i = 0; i < A.length(); i++) {
+            count[A.charAt(i)]++;
+            count[B.charAt(i)]--;
         }
+ 
+        // Check if all counts become 0
+        for (i = 0; i < 256; i++)
+            if (count[i] != 0)
+                return -1;
+ 
+        i = A.length() - 1;
+        j = B.length() - 1;
         
-        for(int i =0; i<A.length(); i++){
-            arr[A.charAt(i)]++;
-            arr[B.charAt(i)]--;
-        }
-        
-        for(int i=0; i<256; i++){
-            if(arr[i] != 0) return -1;
-        }
-        
-        int i =A.length()-1;
-        int j = i;
-        int res = 0;
-        while(i>=0){
+        String ans = "";
+        String front = "";
+        while( i >= 0){
             
-            if(A.charAt(i) !=  B.charAt(j)){
-                res++;
+            if(A.charAt(i) != B.charAt(j)){
+                
+                
+                cnt++;
                 i--;
-            } else{
+            }
+            else{
+                
+                ans = A.charAt(i) + ans;
                 i--;
                 j--;
             }
-            
         }
         
-        return res;
+        // System.out.println(front);
+        // front =  + ans;
+        // System.out.println(front + " " + j);
+        // int ii = A.length()-1;
+        // int jj = B.length()-1;
         
+        // while(ii >= 0){
+        //     if(front.charAt(ii) == B.charAt(jj)){
+        //         ii--;
+        //         jj--;
+        //     }
+        //     else{front
+        //         return -1;
+        //     }
+        // }
+        
+        return cnt;
     }
 }
