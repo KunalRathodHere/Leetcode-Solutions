46dc93c9dc344cb7b07ca0dffe645c21f7b6bcff
KunalRathodHere <77500698+KunalRathodHere@users.noreply.github.com>
2023-06-09 23:55:12 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -49,39 +49,35 @@ class gfg
 class Solution 
 { 
     //Function to return max value that can be put in knapsack of capacity W.
-    static int knapSack(int W, int wt[], int val[], int n) 
+    int knapSack(int W, int wt[], int val[], int n) 
     { 
-         // your code here
+         // your code here 
+         int[][] dp = new int[n+1][W+1];
+         for(int[] temp: dp) Arrays.fill(temp, -1);
          
-         int[][] dp = new int[n][W+1];
+         int ans = fcn(0, W, wt, val, dp);
+         return ans;
          
-         for(int[] arr: dp){
-             Arrays.fill(arr, -1);
-         }
-         return function(n-1, W, wt, val, dp);
     }
     
-    static int function(int idx, int W, int wt[], int val[], int[][] dp){
+    int fcn(int idx, int W, int wt[], int val[], int[][] dp) {
         
-        if(idx == 0) {
-            
-            if(wt[0] <= W) return val[0];
-            else return 0;
-            
-        }
+        if(W < 0 ) return (int) -1e9;
+        
+        if(W == 0) return 0;
+        if(idx == val.length) return 0;
         
         if(dp[idx][W] != -1) return dp[idx][W];
         
-        int notTake = function(idx -1, W, wt, val, dp);
-        int take = Integer.MIN_VALUE;
+        int take, nottake;
         
-        if(wt[idx] <= W){
-            take= val[idx] + function(idx -1, W - wt[idx], wt, val, dp);
-        }
+        take= fcn(idx +1, W - wt[idx], wt, val, dp) + val[idx];
+        nottake= fcn(idx+1, W, wt, val, dp);
         
-        return dp[idx][W] =  Math.max(notTake, take);
+        return dp[idx][W] = Math.max(take, nottake);
         
     }
+    
 }
 
 
