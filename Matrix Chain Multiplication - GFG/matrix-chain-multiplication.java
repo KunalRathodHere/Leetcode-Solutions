300c5a3fe78bde10effd7df9a82f66f5d09379bc
KunalRathodHere <77500698+KunalRathodHere@users.noreply.github.com>
2023-06-23 16:21:48 +0530

    Added solution - LeetHub

---
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.matrixMultiplication(N, arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+     int matrixMultiplication(int N, int arr[])
+    {
+        // code here
+        int[][] dp = new int[N][N];
+        for(int[] temp: dp) Arrays.fill(temp, -1);
+        
+        return mulitply(1, N-1, arr, dp);
+        
+    }
+    
+    int mulitply(int start, int end, int[] arr, int[][] dp ) {
+        
+        if(start == end) return 0;
+        
+        if(dp[start][end] != -1) return dp[start][end];
+        
+        int min_steps = (int) 1e9;
+        
+        for(int k = start; k<= end -1; k++){
+            
+            int partition_one_stps = mulitply(start, k, arr, dp);
+            int partition_two_stps = mulitply(k + 1, end, arr, dp);
+            
+            int multiply_paritions = arr[start-1] * arr[k] * arr[end];
+            
+            int totoal = partition_one_stps + partition_two_stps 
+            + multiply_paritions;
+            
+            min_steps = Math.min(min_steps, totoal);
+            
+        }
+        
+        return dp[start][end] = min_steps;
+    }
+    
+}
